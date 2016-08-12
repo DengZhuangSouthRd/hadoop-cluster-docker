8baad9be39b3fa8f83c0999bbcb6ef5296f81000
liuguiyangnwpu <liuguiyangnwpu@163.com>
2016-08-12 13:15:23 +0800

    修改了Docker可以挂载外部目录，但是还不能自动配置静态IP

--- start-container.sh
+++ start-container.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# kill already run docker
+sudo docker rm -f `sudo docker ps -a -q`
+
 # the default node number is 3
 N=${1:-3}
 
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+	echo "start hadoop-slave$i container..."
+	sudo docker run -itd \
+	                --net=hadoop \
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+                    -v /opt/hadoop/slave$i/datanode:/root/hdfs/datanode \
+                    -v /opt/hadoop/slave$i/namenode:/root/hdfs/namenode \
+                    liuguiyangnwpu/hadoop:slave$i &> /dev/null
+	i=$(( $i + 1 ))
+done 
 
 # start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
@@ -13,22 +29,12 @@ sudo docker run -itd \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+                -v /opt/hadoop/master/datanode:/root/hdfs/datanode \
+                -v /opt/hadoop/master/namenode:/root/hdfs/namenode \
+                liuguiyangnwpu/hadoop:master &> /dev/null
 
 
-# start hadoop slave container
-i=1
-while [ $i -lt $N ]
-do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
-	echo "start hadoop-slave$i container..."
-	sudo docker run -itd \
-	                --net=hadoop \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
-	i=$(( $i + 1 ))
-done 
 
+sudo docker ps -a
 # get into hadoop master container
 sudo docker exec -it hadoop-master bash
